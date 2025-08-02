fc806e37dc7df0a8a5520d520431d688fc11d911
KimSooHyo <138954201+KimSooHyo@users.noreply.github.com>
2025-08-02 14:39:08 +0900

    [level 3] Title: 베스트앨범, Time: 1.00 ms, Memory: 9.33 MB -BaekjoonHub

--- Python3/프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ Python3/프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,23 +1,39 @@
 def solution(genres, plays):
     answer = []
+    dict = {}
+    for v, g in enumerate(genres):
+        if g not in dict:
+            dict[g] = plays[v]
+        else:
+            dict[g] += plays[v]
+    print(dict)
     
-    genres_dict = {}
-    plays_dict = {}
+    genre = set(genres)
+    print(genre)
+    genre_play = []
+    for i in range(len(genres)):
+        genre_play.append([genres[i], plays[i], dict[genres[i]], i])
+    print(genre_play)
     
-    for i in range(len(plays)):
-        g = genres[i]    
-        p = plays[i]
-        if g not in genres_dict:
-            genres_dict[g] = []
-            plays_dict[g] = 0
-        
-        genres_dict[g].append((i, p))
-        plays_dict[g] += p
-        
-    sorted_genres = sorted(plays_dict.items(), key = lambda x : x[1], reverse=True)
     
-    for genre, _ in sorted_genres:
-        sorted_songs = sorted(genres_dict[genre], key = lambda x : (-x[1], x[0]))
-        answer.extend([idx for idx, _ in sorted_songs[:2]])
+    sorted_list = sorted(genre_play, key=lambda x: ((-x[2]), -x[1], x[3]))
+    
+    g = []
+
+    for i in range(len(sorted_list)):
+        print("i:", i)
+        print("answer", answer)
+        print(len(answer), (len(genre) * 2))
+        print("g : ",g)
+        if len(answer) >= (len(genre) * 2):
+            print(len(answer), (len(genre) * 2))
+            return answer
         
+        print("g count : ",g.count(sorted_list[i][0]))
+        if g.count(sorted_list[i][0]) >= 2:
+            continue
+        print("append suc")
+        answer.append(sorted_list[i][3])
+        g.append(sorted_list[i][0])
+    
     return answer
