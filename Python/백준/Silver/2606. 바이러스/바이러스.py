c3102dbc36abde480c94ebbefd83aa50bfaf130f
KimSooHyo <138954201+KimSooHyo@users.noreply.github.com>
2025-08-10 22:26:23 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- Python/백준/Silver/2606. 바이러스/바이러스.py
+++ Python/백준/Silver/2606. 바이러스/바이러스.py
@@ -2,30 +2,31 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-computer = int(input())
+com = int(input())
 n = int(input())
-
-graph = [[] for _ in range(computer+1)]
-for i in range(n):
+graph = [[] for _ in range(com+1)]
+for _ in range(n):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
     
-visited = [0] * (computer+1)
-
-def dfs(start_node):
-    global cnt
-    visited[start_node] = 1
-    queue = deque([start_node])
+visited = [False] * (com+1)
+def bfs(start):
+    visited[start] = True
+    cnt = 0
+    
+    queue = deque()
+    queue.append(start)
     
     while queue:
         node = queue.popleft()
-
-        for next_node in graph[node]:
-            if visited[next_node] == 0:
-                visited[next_node] = 1
-                queue.append(next_node)
+        
+        for next in graph[node]:
+            if graph[next] and not visited[next]:
+                queue.append(next)
+                visited[next] = True
+                cnt += 1
                 
-         
-dfs(1)       
-print(sum(visited)-1)
+    return cnt
+
+print(bfs(1))
