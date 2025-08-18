092bcc082ca2a763493d29532f9c1a00bf8a053d
KimSooHyo <138954201+KimSooHyo@users.noreply.github.com>
2025-08-19 00:58:54 +0900

    [level 2] Title: 땅따먹기, Time: 410.39 ms, Memory: 31.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ Python3/프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,7 +1,7 @@
 def solution(land):
-    #각 행마다 이전 행에서의 최대 점수를 더해가며 최대 점수를 구함
-    for i in range(1, len(land)):
+    l = len(land)
+    for i in range(1, l):
         for j in range(4):
-            #이전 행에서 현재 열의 값을 제외한 나머지 열들 중에서 가장 큰 값을 더함
-            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+            land[i][j] += max(land[i-1][k] for k in range(4) if k != j)
+
     return max(land[-1])
