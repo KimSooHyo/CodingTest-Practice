bb3f5c376cb40fcfb0ee622f88f1950f47e4bbe9
KimSooHyo <138954201+KimSooHyo@users.noreply.github.com>
2025-08-28 15:43:43 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 480 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -0,0 +1,15 @@
+nums = list(map(int, input().split()))
+nums.sort()
+n = nums[2]
+
+while True:
+    cnt = 0
+    for num in nums:
+        if n % num == 0:
+            cnt += 1
+    
+    if cnt >= 3:
+        print(n)
+        break
+    
+    n += 1
