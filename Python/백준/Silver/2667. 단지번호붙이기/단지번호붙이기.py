f0ea222210ed5a443509230f3ad322bf56e33d8b
KimSooHyo <138954201+KimSooHyo@users.noreply.github.com>
2025-09-24 16:55:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,42 +1,43 @@
-import sys
 from collections import deque
+import sys
+sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
 n = int(input())
-graph = [[0] * n for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
+grid = [[] for _ in range(n)]
 for i in range(n):
-    graph[i] = list(input().strip())
+    grid[i] = list(input().strip())
+answer = []
 
-def bfs(x, y):
+def bfs(y, x):
     queue = deque()
-    queue.append((x,y))
-    visited[y][x] = True
+    queue.append((y,x))
+    grid[y][x] = '0'
     cnt = 1
     
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
     while queue:
-        cx, cy = queue.popleft()
+        y, x = queue.popleft()
+        
         for i in range(4):
-            nx = cx + dx[i]
-            ny = cy + dy[i]
-            
+            nx = x + dx[i]
+            ny = y + dy[i]
+        
             if 0 <= nx < n and 0 <= ny < n:
-                if graph[ny][nx] == '1' and visited[ny][nx] == False:
-                    visited[ny][nx] = True
-                    queue.append((nx, ny))
-                    cnt += 1          
-                    
+                if grid[ny][nx] == '1':
+                    queue.append((ny, nx))
+                    grid[ny][nx] = '0'
+                    cnt += 1
+    
     return cnt
 
-total_cnt = []
 for i in range(n):
     for j in range(n):
-        if graph[j][i] == '1' and visited[j][i] == False:
-            total_cnt.append(bfs(i, j))
-        
-print(len(total_cnt))
-for num in sorted(total_cnt):
-    print(num)
+        if grid[i][j] == '1':   # (i,j) 순서 주의
+            answer.append(bfs(i, j))
+
+print(len(answer))             # 총 단지 수
+for x in sorted(answer):       # 오름차순 출력
+    print(x)
+            
