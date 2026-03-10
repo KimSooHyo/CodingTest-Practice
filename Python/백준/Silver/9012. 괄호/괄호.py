0a47b3c89ca0e568890e11d22f35855acd3f435d
KimSooHyo <138954201+KimSooHyo@users.noreply.github.com>
2026-03-10 22:01:35 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/9012. 괄호/괄호.py
+++ Python/백준/Silver/9012. 괄호/괄호.py
@@ -1,28 +1,18 @@
-import sys
+n = int(input())
 
-def solution(text):
-    for t in text:
-        if t == '(':
-            stack.append(t)
-        elif t == ')':
-            if not stack:
-                return False
-            else:
-                stack.pop()
-    
-    if stack:
-        return False
-    else:
-        return True
-
-n = int(sys.stdin.readline().strip())
 for _ in range(n):
     stack = []
-    
-    text = sys.stdin.readline().strip()
-    answer = solution(text)
-    
-    if answer:
-        print("YES")
-    else:
+    line = input().strip()
+    for l in line:
+        if not stack:
+            stack.append(l)
+        else:
+            if l == ")" and stack[-1] == "(":
+                stack.pop()
+            else:
+                stack.append(l)
+
+    if stack:
         print("NO")
+    else:
+        print("YES")
